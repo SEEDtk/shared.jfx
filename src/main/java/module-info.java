7db5197fe71752ca96486b153a1e4a150de3a1aa
Bruce Parrello <bparrello@figresearch.com>
2023-12-01 11:14:53 -0600

    SLF4J upgrade

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,12 +1,11 @@
 module org.theseed.shared.jfx {
     requires transitive javafx.controls;
     requires javafx.fxml;
-	requires java.prefs;
-	requires slf4j.api;
-	requires shared;
-	requires commons.math3;
-	requires basic;
-
+    requires shared;
+    requires commons.math3;
+    requires basic;
+    requires java.prefs;
+    requires org.slf4j;
     opens org.theseed.shared.jfx to javafx.fxml;
     exports org.theseed.shared.jfx;
 }
